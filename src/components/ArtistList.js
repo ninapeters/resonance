9621f60d3f7a84f5622b2e6fde01cea317c1dd80
Nina Peters <peters.nina@outlook.de>
2020-11-17 12:14:38 +0100

    Add ArtistList component w/ styleguidist

---
+++ src/components/ArtistList.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+//import PropTypes from 'prop-types'
+
+//ArtistList.propTypes = {}
+
+export default function ArtistList() {
+  return <h2>Artist List</h2>
+}
