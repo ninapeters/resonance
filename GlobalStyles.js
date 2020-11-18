59e71626cf93377636bfb4d46a2ea65dd55fcf9e
Nina Peters <peters.nina@outlook.de>
2020-11-18 09:11:57 +0100

    Add GlobalStyles

---
+++ GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box;
+    font-family: sans-serif;
+}
+
+body {
+    margin: 0;
+    font-size: 112.5%;
+}
+`
