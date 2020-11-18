c2112411ea92036c48cc91367394792c48a28e67
Nina Peters <peters.nina@outlook.de>
2020-11-18 12:17:51 +0100

    Add state and render track data

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,19 +1,59 @@
-//import styled from 'styled-components'
-import data from '../data/artistList.json'
-
+import styled from 'styled-components/macro'
 //import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react'
+import getTrackData from '../services/getTrackData'
+
+export default ArtistList
+
 //ArtistList.propTypes = {}
 
-export default function ArtistList() {
-  const artists = data.artists.items
+function ArtistList() {
+  const [trackData, setTrackData] = useState([])
+
+  useEffect(() => {
+    getTrackData().then((tracks) => setTrackData(tracks.items))
+  }, [])
+
+  const artistDataList = trackData.map((item) => {
+    return {
+      artist: item.artists[0].name,
+      songTitle: item.name,
+      id: item.id,
+    }
+  })
 
   return (
     <div>
-      {artists.map(({ id, name }) => (
-        <div>
-          <span key={id}>{name}</span>
-        </div>
-      ))}
+      <ListStyled>
+        {artistDataList.map(({ artist, songTitle, id }) => (
+          <ListItemStyled key={id}>
+            <span>{artist}</span>
+            {songTitle}
+          </ListItemStyled>
+        ))}
+      </ListStyled>
     </div>
   )
 }
+
+const ListStyled = styled.ul`
+  display: grid;
+  font-family: sans-serif;
+  gap: 8px;
+  list-style: none;
+  padding: 0;
+`
+const ListItemStyled = styled.li`
+  span {
+    display: block;
+    font-size: 75%;
+    padding-bottom: 6px;
+    text-transform: uppercase;
+  }
+
+  background-color: #e6eded;
+  color: #2e3332;
+  font-size: 1.2em;
+  font-weight: 600;
+  padding: 20px;
+`
