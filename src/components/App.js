4382a8046776dc72e9f9c386d06231802b0cd1e9
Nina Peters <peters.nina@outlook.de>
2020-11-25 20:14:19 +0100

    Add navigation button and update file structure

--- src/components/App.js
+++ src/components/App.js
@@ -6,43 +6,73 @@ import useAudio from './hooks/useAudio'
 import useArtist from './hooks/useArtist'
 import ArtistPage from './Pages/ArtistPage/ArtistPage'
 import SavedSongPage from './Pages/SavedSongPage/SavedSongPage'
+import Navigation from './Navigation/Navigation'
 
 export default App
 
 function App() {
   const [artistData, setArtistData] = useState([])
-
   useEffect(() => {
     setArtistData(normalizeArtists(data.tracks.items))
   }, [])
-
   const { toggleCurrentSongId, isSongPlaying, currentSongId } = useAudio({
     artistData,
   })
-
   const { savedSongs, saveSong } = useArtist({ artistData })
+  const [showSavedSongPage, setShowSavedSongPage] = useState(false)
 
   return (
-    <div>
+    <AppWrapper>
       <UnmuteMessage>Don't forget to unmute your device.</UnmuteMessage>
-      <ArtistPage
-        artists={artistData}
-        toggleCurrentSongId={toggleCurrentSongId}
-        isSongPlaying={isSongPlaying}
-        currentSongId={currentSongId}
-        saveSong={saveSong}
-        savedSongs={savedSongs}
-      />
-      <SavedSongPage
-        savedSongs={savedSongs}
-        toggleCurrentSongId={toggleCurrentSongId}
-        isSongPlaying={isSongPlaying}
-        currentSongId={currentSongId}
-      />
-    </div>
+      <Main>
+        {showSavedSongPage === false ? (
+          <ArtistPage
+            artists={artistData}
+            toggleCurrentSongId={toggleCurrentSongId}
+            isSongPlaying={isSongPlaying}
+            currentSongId={currentSongId}
+            saveSong={saveSong}
+            savedSongs={savedSongs}
+          />
+        ) : (
+          ''
+        )}
+        {showSavedSongPage ? (
+          <SavedSongPage
+            savedSongs={savedSongs}
+            toggleCurrentSongId={toggleCurrentSongId}
+            isSongPlaying={isSongPlaying}
+            currentSongId={currentSongId}
+          />
+        ) : (
+          ''
+        )}
+      </Main>
+      <Footer>
+        <Navigation
+          handleClick={() => setShowSavedSongPage(!showSavedSongPage)}
+        >
+          {showSavedSongPage ? 'all songs' : 'saved songs'}
+        </Navigation>
+      </Footer>
+    </AppWrapper>
   )
 }
-
+const AppWrapper = styled.div`
+  display: grid;
+  grid-template-rows: 6% auto 8%;
+  height: 100vh;
+`
+const Main = styled.main`
+  overflow-y: auto;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+const Footer = styled.footer`
+  place-self: center;
+`
 const UnmuteMessage = styled.p`
   padding: 4px;
   color: var(--primary-dark-transparent);
--- src/components/Button.md
+++
@@ -1,9 +0,0 @@
-Regular Button
-
-```js
-<Button>Save this song</Button>
-```
-
-```js
-<Button disabled>Save this song</Button>
-```
