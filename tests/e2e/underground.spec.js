286f20c5a01596eabaf04b3ed52a10db2870177f
Nina Peters <peters.nina@outlook.de>
2020-11-25 20:13:32 +0100

    Add cypress and configs

---
+++ tests/e2e/underground.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+const testId = (id) => `[data-testid="${id}"]`
+
+describe('Underground', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  describe('Underground', () => {})
+})
