2094436283f81b1511aab1fdca446b4c84a8622e
Nina Peters <peters.nina@outlook.de>
2020-12-02 11:02:49 +0100

    move react app code and add server files

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require('./api/index')
+const port = process.env.PORT || 3001
+const express = require('express')
+const path = require('path')
+
+// Handle React routing, return all requests to React app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
