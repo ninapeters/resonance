8663bec7e65ed9c203a7ca612dab20ac85b307c1
Nina Peters <peters.nina@outlook.de>
2020-12-02 12:48:20 +0100

    Update variables

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
-const app = require('./api/index')
-const port = process.env.PORT || 8888
+const cors = require("cors")
 const express = require('express')
-const path = require('path')
+const request = require('request')
+const querystring = require('querystring')
 
-// Handle React routing, return all requests to React app
-app.use(express.static(path.join(__dirname, 'client/build')))
+const app = express()
+app.use(cors())
 
+const redirect_uri = 
+  process.env.REDIRECT_URI || 
+  'http://localhost:8888/callback'
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+const client_id = process.env.CLIENT_ID
+console.log(client_id)
+const client_secret = process.env.CLIENT_SECRET
+const port = process.env.PORT || 8888
+
+app.get('/login', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id,
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
 })
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
+app.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        client_id + ':' + client_secret
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    let access_token = body.access_token
+    const uri = 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
 })
 
+console.log(`Server listening at http://localhost:${port}.`)
+app.listen(port)
