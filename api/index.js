c067f86af8c76a1f0131df4894852245daf78bbe
Nina Peters <peters.nina@outlook.de>
2020-12-08 11:03:54 +0100

    Add vercel.json

--- api/index.js
+++ api/index.js
@@ -1,19 +1,11 @@
 const express = require('express')
-const axios = require('axios')
+//const axios = require('axios')
 
 const app = express()
 
-const sendAxiosError = (res) => (error) =>
-  res.status(error.response.status || 500).json(error.response.data)
+/* const sendAxiosError = (res) => (error) =>
+  res.status(error.response.status || 500).json(error.response.data) */
 
-// get profile data
-app.get('/profile', (req, res) => {
-  axios
-    .get('https://api.spotify.com/v1/me')
-    .then((result) => {
-      res.json(result.data)
-    })
-    .catch(sendAxiosError(res))
-})
+// requests
 
 module.exports = app
