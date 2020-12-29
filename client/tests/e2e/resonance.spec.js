b9f67068713646bba1f669042333649ae9e5fe1c
Nina Peters <peters.nina@outlook.de>
2020-12-29 16:06:11 +0100

    Update cypress tests

--- client/tests/e2e/resonance.spec.js
+++ client/tests/e2e/resonance.spec.js
@@ -11,12 +11,26 @@ describe('resonance', () => {
     cy.get('[data-testid=login-button]')
   })
 
-  /*   it('should lead to homepage by clicking the login button', () => {
-    cy.get('[data-testid=login-button]')
-      .should('have.attr', 'href')
-      .and('include', 'login')
-      .then((href) => {
-        cy.visit(href)
-      })
-  }) */
+  it('should lead to homepage by login', () => {
+    const authHeader =
+      'Basic NWY3ZmEzMjRlYzEyNGM1ZGE1NjcyZGQ1ZDZmYTliMmY6NjhhYmRmZThiNGU5NDk5NmI4ZmNiNzJjNzM3NDk4MTM='
+
+    const options = {
+      method: 'POST',
+      url: 'https://accounts.spotify.com/api/token',
+      form: true,
+      body: {
+        grant_type: 'client_credentials',
+      },
+      headers: {
+        Authorization: authHeader,
+      },
+    }
+
+    cy.request(options).then((resp) => {
+      localStorage.setItem('token', resp.body.access_token)
+    })
+
+    cy.visit('/#')
+  })
 })
